91c41821beadefe185ecbac9305a56afa3d9a649
Robin Lichtenthaeler <robin.lichtenthaeler@web.de>
2019-02-28 19:35:57 +0100

    corrected parser

--- src/problems/pizza/PizzaSolver.java
+++ src/problems/pizza/PizzaSolver.java
@@ -1,18 +1,18 @@
 package problems.pizza;
 
-import java.util.Arrays;
-
 import template.Solver;
 
 public class PizzaSolver extends Solver {
 
-    public PizzaSolution solve(PhotoProblem problem) {
+	public PizzaSolution solve(PhotoProblem problem) {
 
-	// TODO This is only a hard coded solution for a_example.in problem
-	PizzaSolution solution = new PizzaSolution();
-	solution.numberOfSlices = 3;
-	solution.slices = Arrays.asList("0 0 2 1", "0 2 2 2", "0 3 2 4");
+		// TODO This is only a hard coded solution for a_example.in problem
+		PizzaSolution solution = new PizzaSolution();
+		// solution.numberOfSlices = 3;
+		// solution.slices = Arrays.asList("0 0 2 1", "0 2 2 2", "0 3 2 4");
 
-	return solution;
-    }
+		System.out.println(problem.toString());
+
+		return solution;
+	}
 }
