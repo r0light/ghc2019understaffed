d751c67c39f81cb474c230220544d3c4101d389f
Jan Boockmann <Jan.Boockmann@gmail.com>
2019-02-28 21:15:08 +0100

    savepoint

--- src/problems/pizza/PizzaMain.java
+++ src/problems/pizza/PizzaMain.java
@@ -10,15 +10,28 @@ public class PizzaMain {
 	public static void main(String[] args) throws Exception {
 		System.out.println("Running Main ...");
 
-		final Path input = Paths.get("problems/pizza/b_lovely_landscapes.txt");
-		final Path output = Paths.get("problems/pizza/b_lovely_landscapes.out");
+		// final Path input = Paths.get("problems/pizza/a_example.txt");
+		// final Path output = Paths.get("problems/pizza/a_example.out");
+
+		// final Path input = Paths.get("problems/pizza/b_lovely_landscapes.txt");
+		// final Path output = Paths.get("problems/pizza/b_lovely_landscapes.out");
+
+		// final Path input = Paths.get("problems/pizza/c_memorable_moments.txt");
+		// final Path output = Paths.get("problems/pizza/c_memorable_moments.out");
+
+		// final Path input = Paths.get("problems/pizza/d_pet_pictures.txt");
+		// final Path output = Paths.get("problems/pizza/d_pet_pictures.out");
+
+		final Path input = Paths.get("problems/pizza/e_shiny_selfies.txt");
+		final Path output = Paths.get("problems/pizza/e_shiny_selfies.out");
 
 		final PhotoProblem problem = new PhotoParser().parse(input);
-		final PizzaSolver solver = new PizzaSolver();
+		final PizzaSolver2 solver = new PizzaSolver2();
 		final PizzaSolution solution = solver.solve(problem);
 		final Writer writer = new PizzaWriter();
 		writer.write(solution, output);
 		System.out.println(String.format("%s => %s", input, output));
+		System.out.println(solution.score());
 
 	}
 }
--- src/problems/pizza/PizzaSolver2.java
+++ src/problems/pizza/PizzaSolver2.java
@@ -1,6 +1,8 @@
 package problems.pizza;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,28 +11,49 @@ import template.Solver;
 public class PizzaSolver2 extends Solver {
 
 	public PizzaSolution solve(PhotoProblem problem) {
-		List<Slide> slides = problem.photos.stream().map(x -> {
+
+		List<Photo> Hphotos = problem.photos.stream().filter(x -> x.orientation == Orientation.H)
+				.collect(Collectors.toList());
+
+		List<Slide> Hslides = Hphotos.stream().map(x -> {
 			List<Photo> photos = new ArrayList<Photo>();
 			photos.add(x);
 			Slide s = new Slide(photos);
 			return s;
 		}).collect(Collectors.toList());
 
+		List<Photo> Vphotos = problem.photos.stream().filter(x -> x.orientation == Orientation.V)
+				.collect(Collectors.toList());
+		
+		// TODO: EY idea merge photos with small intersect.
+
+		assert Vphotos.size() % 2 == 0;
+		List<Slide> Vslides = new ArrayList<Slide>();
+		for (int i = 0; i < Vphotos.size(); i += 2) {
+			Photo a = Vphotos.get(i);
+			Photo b = Vphotos.get(i + 1);
+			Slide slide = new Slide(Arrays.asList(a, b));
+			Vslides.add(slide);
+		}
+
 		PizzaSolution sol = new PizzaSolution();
+		List<Slide> slides = new ArrayList<Slide>(Vslides);
+		Collections.copy(slides, Vslides);
+		slides.addAll(Hslides);
 		sol.slides = slides;
 
-		int score = 0;
-		Slide last = null;
-		for (Slide s : sol.slides) {
-			if (last == null) {
-				last = s;
-				continue;
+		int score = sol.score();
+		int maxScore = score;
+		System.out.println(score);
+		while (score <= 100000) {
+			Collections.shuffle(slides);
+			score = sol.score();
+			if (score > maxScore) {
+				maxScore = score;
 			}
-			int newScore = Eval.score(last, s);
-			score += newScore;
-			last = s;
+			System.out.println("SCORE: " + score + "--max so far: " + maxScore);
 		}
-		System.out.println("SCORE: " + score);
+		System.out.println("DONE");
 
 		return sol;
 	}
